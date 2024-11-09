e9776ebdd4ae1739bd2b4235cbb2274891938e49
JLS <15868811005@163.com>
2024-11-09 22:43:45 +0800

    代码上传

--- basic_demo/type_demo.py
+++ basic_demo/type_demo.py
@@ -2,3 +2,18 @@
 
 if __name__ == '__main__':
     print(type(100))
+    print(type(1+4j))
+    if 4 > 3:
+        print('4>3')
+    elif 4 < 3:
+        print('4<3')
+    else :
+        print('4=3')
+
+    for string in 'hello world':
+        print(string)
+
+    i = 1
+    while i < 10:
+        print(i)
+        i += 1
