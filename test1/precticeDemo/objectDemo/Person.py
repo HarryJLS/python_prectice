1e1666fec2a4af1480c9d7602074d1758a1400f7
Jiangls <jls15868811005@gmail.com>
2024-11-25 17:02:36 +0800

    代码更新

---
+++ test1/precticeDemo/objectDemo/Person.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def print_info(self):
+        print(f'name: {self.name[0]}')
+        print(f'age: {self.age}')
+
+
+if __name__ == "__main__":
+
+    o = Person(["1", "2", "3"], 18)
+    o.print_info()
